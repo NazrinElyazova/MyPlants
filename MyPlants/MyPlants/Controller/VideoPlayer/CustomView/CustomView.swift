e8fd23afb16520f05f277b9eab31bbe70f51f924
NazrinElyazova <148608981+NazrinElyazova@users.noreply.github.com>
2024-04-08 11:03:57 +0400

    Go forward 5 button and go back 5 button configuration

--- MyPlants/MyPlants/Controller/VideoPlayer/CustomView/CustomView.swift
+++ MyPlants/MyPlants/Controller/VideoPlayer/CustomView/CustomView.swift
@@ -10,17 +10,17 @@ import AVFoundation
 
 protocol TimeDelegate {
     func setVideoTime(result: Float)
+    func gobackVideo(seconds: Double)
+    func goForwardVideo(seconds: Double)
 }
 
 final class CustomView: UIView {
     
     var delegate: TimeDelegate?
-
-    @IBOutlet weak var slider: UISlider!
     var player: AVPlayer?
-    var playerDuration: CMTime?
- 
     var timeObserverToken: Any?
+    
+    @IBOutlet weak var slider: UISlider!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,56 +28,62 @@ final class CustomView: UIView {
     }
     
     @IBAction func sliderAction(_ sender: UISlider) {
-      
-                delegate?.setVideoTime(result: Float(sender.value))
+        delegate?.setVideoTime(result: Float(sender.value))
     }
     
-   
     @IBAction func playAndPauseButtonAction(_ sender: Any) {
         if let player = player {
             if player.rate == 0 {
                 player.play()
-                
             } else {
                 player.pause()
             }
         }
     }
+    @IBAction func goForwardButton(_ sender: Any) {
+        delegate?.goForwardVideo(seconds: 5)
+        
+    }
     
+    @IBAction func goBackButton(_ sender: Any) {
+        delegate?.gobackVideo(seconds: 5)
+    }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.configureView()
     }
+    
     deinit {
-          if let token = timeObserverToken {
-              player?.removeTimeObserver(token)
-              timeObserverToken = nil
-          }
-      }
+        if let token = timeObserverToken {
+            player?.removeTimeObserver(token)
+            timeObserverToken = nil
+        }
+    }
     
     private func configureView() {
         guard let view = self.loadFromNib(nibName: "CustomView") else {return}
         view.frame = self.bounds
         self.addSubview(view)
     }
+    
     func setupPlayer(player: AVPlayer) {
-           self.player = player
-           addPeriodicTimeObserver()
-       }
-
+        self.player = player
+        addPeriodicTimeObserver()
+    }
+    
     private func addPeriodicTimeObserver() {
-           let timeScale = CMTimeScale(NSEC_PER_SEC)
-           let time = CMTime(seconds: 1, preferredTimescale: timeScale)
-           
-           timeObserverToken = player?.addPeriodicTimeObserver(forInterval: time, queue: .main) { [weak self] time in
-               guard let self = self else { return }
-               let currentTime = CMTimeGetSeconds(time)
-               let duration = CMTimeGetSeconds(self.player?.currentItem?.duration ?? CMTime.zero)
-               let progress = Float(currentTime / duration)
-               self.slider.value = progress
-           }
-       }
+        let timeScale = CMTimeScale(NSEC_PER_SEC)
+        let time = CMTime(seconds: 1, preferredTimescale: timeScale)
+        
+        timeObserverToken = player?.addPeriodicTimeObserver(forInterval: time, queue: .main) { [weak self] time in
+            guard let self = self else { return }
+            let currentTime = CMTimeGetSeconds(time)
+            let duration = CMTimeGetSeconds(self.player?.currentItem?.duration ?? CMTime.zero)
+            let progress = Float(currentTime / duration)
+            self.slider.value = progress
+        }
+    }
     
 }
 
--- MyPlants/MyPlants/Controller/VideoPlayer/VideoPlayerController.swift
+++ MyPlants/MyPlants/Controller/VideoPlayer/VideoPlayerController.swift
@@ -12,27 +12,27 @@ import AVFoundation
 class VideoPlayerController: UIViewController {
     
     var player: AVPlayer?
+    
     @IBOutlet weak var video: UIView!
     @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var customView: CustomView! {
         didSet {
             customView.delegate = self
-            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         playTheVideo()
+        videoView.layer.cornerRadius = 20
+        videoView.clipsToBounds = true
     }
-  
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         player?.pause()
-        
     }
+    
     func playTheVideo() {
         player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "AloeProduct", ofType: "mov")!))
         let layer = AVPlayerLayer(player: player)
@@ -40,13 +40,26 @@ class VideoPlayerController: UIViewController {
         layer.videoGravity = .resizeAspectFill
         videoView.layer.addSublayer(layer)
         player?.play()
-        
-        
         customView.setupPlayer(player: player!)
+    }
+    
+    private func seekVideo(seconds: Double) {
+        guard let player = player else { return }
+        
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        let newTime = currentTime + seconds
+        
+        let duration = CMTimeGetSeconds(player.currentItem?.duration ?? CMTime(seconds: 1, preferredTimescale: 1))
+        
+        if newTime > 0 && newTime < duration {
+            let time = CMTime(seconds: newTime, preferredTimescale: 1)
+            player.seek(to: time)
+        }
     }
 }
 
 extension VideoPlayerController: TimeDelegate {
+    
     func setVideoTime(result: Float) {
         if let player = player {
             let duration = CMTimeGetSeconds(player.currentItem!.duration)
@@ -54,5 +67,15 @@ extension VideoPlayerController: TimeDelegate {
             let time = CMTime(seconds: seekTime, preferredTimescale: 1)
             player.seek(to: time)
         }
+    }
+    
+    func gobackVideo(seconds: Double) {
+        seekVideo(seconds: -seconds)
+        
+    }
+    
+    func goForwardVideo(seconds: Double) {
+        seekVideo(seconds: seconds)
+        
     }
 }
